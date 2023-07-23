c5b1d857ae4d9bb463689951fa2cacf999c3719a
Rimsha iqbal <rimshaiqbal583@gmail.com>
2023-07-22 22:59:03 -0400

    Personal Portfolio first commit

---
+++ script.js
@@ -0,0 +1,59 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+   
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+    });
+
+    // owl animation script
+    var typed = new Typed(".typing",{
+        strings:["Web Developer","Designer","Front-End Developer"],
+        typeSpeed: 100,
+        backspeed: 60,
+        loop: true
+        
+    });
+     
+    var typed = new Typed(".typing-2",{
+        strings:["I am currently studying ","Web Technology Specialist","at TriOS College"],
+        typeSpeed: 100,
+        backspeed: 60,
+        loop: true
+    });
+    
+    var typed = new Typed(".typing-3",{
+        strings:["Technical Skills ", "Course Load"],
+        typeSpeed: 100,
+        backspeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-4",{
+        strings:["Feel Free To ", "Get In Touch"],
+        typeSpeed: 100,
+        backspeed: 60,
+        loop: true
+    });
+
+    //toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+});
+
+
+
+
